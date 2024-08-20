6c98c4dc52e28e9d82090cd5b99ebc244e956f5b
MukhametDev <zhanbekovm@mail.ru>
2024-08-20 18:56:32 +0500

    second changes

--- src/index.php
+++ src/index.php
@@ -29,7 +29,7 @@ $url = "https://www.1c-bitrix.ru/partners/index_ajax.php";
 //print_r($html);
 //$parser->parsAllPartners();
 $DB = new CDatabase();
-// $file = fopen('recap_data.txt', 'r');
+// $file = fopen('newdata.txt', 'r');
 
 // while (!feof($file)) {
 //     $line = fgets($file);
@@ -51,9 +51,11 @@ $DB = new CDatabase();
 // }
 
 // fclose($file);
-
-$file = fopen('partners.txt', 'r');
+$file = fopen('projects.txt', 'r');
 $filewriter = fopen('newdata.txt', 'w');
+
+$buffer = '';
+$inRow = false;
 
 while (!feof($file)) {
     $line = fgets($file);
@@ -61,9 +63,49 @@ while (!feof($file)) {
         continue;
     }
 
-    $arr = explode(':', $line);
-    dd($arr);
+    // Начинаем буферизацию при нахождении начального тега <row>
+    if (strpos($line, '<row>') !== false) {
+        $inRow = true;
+        $buffer = $line;
+    } elseif ($inRow) {
+        // Добавляем строки в буфер, пока не найдем конечный тег </row>
+        $buffer .= $line;
+        if (strpos($line, '</row>') !== false) {
+            $inRow = false;
+
+            // Удаляем начальный и конечный тег <row>
+            $buffer = trim($buffer, "<row>");
+
+            // Разделяем строку по разделителю <#>
+            $arr = explode('<#>', $buffer);
+
+            // Проверяем, что массив содержит как минимум 4 элемента
+            if (count($arr) >= 4) {
+                $id = trim($arr[0]);
+                $site = trim($arr[1]);
+                $category = trim($arr[2]);
+                $description = trim($arr[3]);
+
+                // Добавляем http к названию сайта
+                if (!preg_match('/^http(s)?:\/\//', $site)) {
+                    $site = 'http://' . $site;
+                }
+                $description = html_entity_decode($description, ENT_QUOTES, 'UTF-8');
+                $description = preg_replace('/&nbsp;/', ' ', $description);
+                $description = preg_replace('/[\x00-\x1F\x7F]/', '', $description); // Удаляем непечатные символы
+                // Записываем данные в новый файл
+                $newLine = "{$id}, {$site}, {$category}, {$description}\n";
+                // dd($newLine);
+                fwrite($filewriter, $newLine);
+            }
+
+            // Очищаем буфер
+            $buffer = '';
+        }
+    }
 }
 
+fclose($file);
+fclose($filewriter);
 // $PARTNERMODEL = new PartnerModel();
 //$API = new CApi();
